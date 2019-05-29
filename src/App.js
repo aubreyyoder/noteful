dd0079eb9a92f42872801cd599823f1a3d15cb56
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-05-28 19:23:26 -0700

    Added full application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import Header from "./Header";
+import Main from "./Main";
+import Folder from "./Folder";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    folders: [],
+    notes: []
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <Header />
+        </header>
+        <main />
+        <section />
+      </div>
+    );
+  }
 }
 
 export default App;
